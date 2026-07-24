ac05764f0c206fca5829505c51309ccb44e26e7d
KianS-0 <130585417+KianS-0@users.noreply.github.com>
2026-07-24 13:56:51 +0000

    Enable SQLite foreign key enforcement

--- database.js
+++ database.js
@@ -3,9 +3,22 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("gigtracker.db", (err) => {
   if (err) {
     console.error("Could not connect to database:", err.message);
-  } else {
-    console.log("Connected to SQLite database.");
+    return;
   }
+
+  console.log("Connected to SQLite database.");
+
+  db.run("PRAGMA foreign_keys = ON", (pragmaErr) => {
+    if (pragmaErr) {
+      console.error(
+        "Could not enable foreign keys:",
+        pragmaErr.message
+      );
+      return;
+    }
+
+    console.log("Foreign key enforcement enabled.");
+  });
 });
 
 module.exports = db;
